b67ab8a0914bb037e094c14ca072a41d5ec01cc1
HassanBahati <mukisabahati@gmail.com>
2021-03-10 09:58:38 +0300

    created server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hello, World, Node is working');
+// console.log('Hello, World, Node is working');
+
+//importing the express library
+const express = require('express');
+
+//instantiating express in constant app
+const app = express();
+
+//server to run at port 3000
+app.listen(3000, () => console.log('listening on port 3000'));
