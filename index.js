32003e1ff9c19dde33b0650fe462efcda4b610a6
HassanBahati <mukisabahati@gmail.com>
2021-03-26 01:13:22 +0300

    :rocket:

--- index.js
+++ index.js
@@ -10,91 +10,85 @@ require('dotenv');
 //instantiating express in constant app
 const app = express();
 
+mongoose.connect(
+  process.env.DB_CONNECTION, 
+  { useNewUrlParser: true }, 
+  () => {console.log('connected to DB');}
+);
 
-//db connection 
-mongoose.connect(process.env.DATABASE, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  });
-  
-  mongoose.connection
-  .on('open', () => {
-    console.log('Mongoose connection open');
-  })
-  .on('error', (err) => {
-    console.log(`Connection error: ${err.message}`);
-  });
+// //db connection
+// mongoose.connect(process.env.DATABASE, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+// mongoose.connection
+//   .on('open', () => {
+//     console.log('Mongoose connection open');
+//   })
+//   .once('error', (err) => {
+//     console.log(`Connection error: ${err.message}`);
+//   });
 
 //middleware
-app.use(express.urlencoded({extended: true}));
-//configurations- setting pug as templete engine 
+app.use(express.urlencoded({ extended: true }));
+//configurations- setting pug as templete engine
 app.set('view engine', 'pug');
 app.set('views', './views');
 
 //custom middleware
 app.use((req, res, next) => {
-    console.log('a new request received aat ' + Date.now());
-    next(); 
-})
+  console.log('a new request received aat ' + Date.now());
+  next();
+});
 
 //middleware for serving static files(csss, js , images)
 app.use(express.static('public'));
 
 // //createEmployee route
-// app.get('/createEmployee', (req, res) => { 
+// app.get('/createEmployee', (req, res) => {
 //     // res.send('Homepage! Hello World.');
 //     //res.sendFile(__dirname + '/index.html')
 //     res.render('createEmployee', {title: 'Employee'});
 // });
 
 //import employee route
-const employeeRoutes = require('./routes/employeeRoute')
+const employeeRoutes = require('./routes/employeeRoute');
 
-//instantiting employee route 
+//instantiting employee route
 app.use('/employee', employeeRoutes);
 
 //orders route
 app.get('/createOrders', (req, res) => {
-    res.render('createOrders', {title: 'create Order'})
-})
-
+  res.render('createOrders', { title: 'create Order' });
+});
 
 //path parameters -used to specify the exact route
 
 //incase a route doesnt exist
 app.get('*', (req, res) => {
-    res.send('the route specified doesnt exist')
-})
+  res.send('the route specified doesnt exist');
+});
 
 //query parameters
 
-
-
 //server to run at port 3000
 app.listen(3000, () => console.log('listening on port 3000'));
-
-
-
-
 
 /**
  * what is crud
  * what are http methods
  */
 
-
-
-
-
 /**
- * when user visits browser at http://localhost:3804/ 
- * --browser conducts a READ operation by sending a GET request to the server 
+ * when user visits browser at http://localhost:3804/
+ * --browser conducts a READ operation by sending a GET request to the server
  * --server receives a GET request and handles itt using the express get() method
  * --this express get() method  has 2 arguments , app.get(path/endpoint, callback function)
  * --the first arugement which is the path/end point appears after the domain name for this case its '/'
- * -- then the second argument is a call back function whith 2 arguments (req,res), 
+ * -- then the second argument is a call back function whith 2 arguments (req,res),
  * --it use   s the res object to send a response back to the browser , it does this by send() method res.send()
- * For Callbacks --we are usign ES^ arrow functions instead of the older way of writing functions  
+ * For Callbacks --we are usign ES^ arrow functions instead of the older way of writing functions
  */
 
 /**
